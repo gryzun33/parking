f4c7ae507ded3a00fef0086d0cd9d4b58d86c05d
gryzun33 <yakushevaolga564@gmail.com>
2025-05-07 08:51:06 +0300

    feat: add handlers for uncaughtexception and unhandledrejection

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,6 +1,4 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { Module, OnModuleInit } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 import { ConfigModule } from '@nestjs/config';
 
@@ -11,7 +9,19 @@ import { ConfigModule } from '@nestjs/config';
     }),
     AuthModule,
   ],
-  controllers: [AppController],
-  providers: [AppService],
 })
-export class AppModule {}
+export class AppModule implements OnModuleInit {
+  onModuleInit() {
+    process.on('uncaughtException', (err) => {
+      console.error(`Uncaught Exception: ${err.message}\n`, err.stack);
+    });
+
+    process.on('unhandledRejection', (reason: any) => {
+      if (reason instanceof Error) {
+        console.error(`Unhandled Rejection: ${reason.message}\n`, reason.stack);
+      } else {
+        console.error(`Unhandled Rejection: ${JSON.stringify(reason)}`);
+      }
+    });
+  }
+}
