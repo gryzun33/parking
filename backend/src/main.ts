535f2a1657e12aef833b60cfc5f455dc19544320
gryzun33 <yakushevaolga564@gmail.com>
2025-05-10 21:38:31 +0300

    feat: add clienturl to cors config

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -22,6 +22,7 @@ async function bootstrap() {
       'http://localhost:5173',
       'http://localhost:5174',
       'http://localhost:4173',
+      'https://parking-online.netlify.app/',
     ],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     credentials: true,
